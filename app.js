9a05d379f9185c41c5749b1093941970d5de5047
Jake Sweger <jsweg13@gmail.com>
2023-06-07 15:38:57 -0400

    Create loop to make 16 divs

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const content1 = document.createElement('div');
-container.appendChild(content1);
-const content2 = document.createElement('div');
-container.appendChild(content2);
-const content3 = document.createElement('div');
-container.appendChild(content3);
+for (let i = 0; i < 16; i++) {
+    const content1 = document.createElement('div');
+    container.appendChild(content1);
+  }
