79e5936528940977902f0192e0d6626dbbdea7fd
Jake Sweger <jsweg13@gmail.com>
2023-06-07 15:20:32 -0400

    Add more divs

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const content = document.createElement('div');
-
-container.appendChild(content);
+const content1 = document.createElement('div');
+container.appendChild(content1);
+const content2 = document.createElement('div');
+container.appendChild(content2);
+const content3 = document.createElement('div');
+container.appendChild(content3);
